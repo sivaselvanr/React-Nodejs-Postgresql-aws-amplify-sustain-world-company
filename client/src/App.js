10514961b9d8e7fa027ead6c2ad8f60d3235ff6c
SIVASELVAN <117008153+sivaselvanr@users.noreply.github.com>
2023-01-18 02:18:55 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,36 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import InputTask from './components/inputtask';
+// import EditTask from './components/edittask';
+// import ListTask from './components/listtask';
+// class App extends React.Component {
+//   render()
+//   {
+//     return(
+//       <div className="body">
+//         <InputTask/>
+//         <ListTask/>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import InputTask from './components/inputtask';
-import EditTask from './components/edittask';
-import ListTask from './components/listtask';
-class App extends React.Component {
-  render()
-  {
-    return(
-      <div className="body">
-        <InputTask/>
-        <ListTask/>
-      </div>
-    )
-  }
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
